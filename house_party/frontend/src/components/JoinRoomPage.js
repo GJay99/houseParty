d7f3d60f949f6bc5a9c25af63ccd8067da9f10a9
Gonza <gonzalojay05@gmail.com>
2026-03-28 20:26:04 +0000

    added functionality for users to join rooms

--- house_party/frontend/src/components/JoinRoomPage.js
+++ house_party/frontend/src/components/JoinRoomPage.js
@@ -1,11 +1,75 @@
 import React, { Component } from "react";
+import { TextField, Button, Grid, Typography, touchRippleClasses } from "@mui/material";
+import { data, Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-export default class JoinRoomPage extends Component {
+class JoinRoomPage extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      roomCode: "",
+      error: ""
+    }
+    this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+    this.roomButtonPressed = this.roomButtonPressed.bind(this);
   }
 
   render() {
-    return <p>This is the Join Room page</p>;
+    return(
+      <Grid container spacing={1}>
+        <Grid size={{xs:12}} align="center">
+          <Typography variant="h4" component="h4">
+            Join a Room
+          </Typography>
+        </Grid>
+        <Grid size={{xs:12}} align="center">
+          <TextField
+            error={this.state.error}
+            label="Code"
+            placeholder="Enter a Room Code"
+            value={this.state.roomCode}
+            helperText={this.state.error}
+            variant="outlined"
+            onChange={this.handleTextFieldChange}
+          />
+        </Grid>
+        <Grid size={{xs:12}} align="center">
+          <Button variant="contained" color="primary" onClick={this.roomButtonPressed}>Enter Room</Button>
+          <Button variant="contained" color="secondary" to="/" component={Link}>Back</Button>
+        </Grid>
+      </Grid>
+    );
   }
+
+  handleTextFieldChange(e){
+    this.setState({
+      roomCode: e.target.value,
+    });
+  }
+
+  roomButtonPressed(){
+    const requestOptions = {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        code:this.state.roomCode
+      })
+    };
+    fetch('/api/join-room', requestOptions).then((response) => {
+      if(response.ok) {
+        this.props.navigate("/room/"+this.state.roomCode)
+      }
+      else {
+        this.setState({error: "Room not found."})
+      }
+    })
+    .catch((error) =>{
+      console.log(error);
+    });
+  }
+  
 }
+export default function JoinRoomPageWrapper(props){
+    const navigate = useNavigate();
+    return <JoinRoomPage {...props} navigate={navigate}/>
+  }
